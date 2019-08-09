f76e2eb6832bbdacc96d7d5e6be74480de2e5585
keyberthRengel <keyrengel24@gmail.com>
2019-08-10 00:08:12 +0200

    inicio

---
+++ precache-manifest.c306a3f042958dea3ee5cb893a9d595c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16875967c11c9bec2fe111ed0e5defed",
+    "url": "/Tienda-Online/index.html"
+  },
+  {
+    "revision": "a6cc35327ffa21c0983c",
+    "url": "/Tienda-Online/static/css/main.5828701b.chunk.css"
+  },
+  {
+    "revision": "4f987228df7803849c2b",
+    "url": "/Tienda-Online/static/js/2.4fb61a4e.chunk.js"
+  },
+  {
+    "revision": "a6cc35327ffa21c0983c",
+    "url": "/Tienda-Online/static/js/main.34931b4d.chunk.js"
+  },
+  {
+    "revision": "d81d2b03ee2fdccf4644",
+    "url": "/Tienda-Online/static/js/runtime~main.06c0877e.js"
+  }
+]);
